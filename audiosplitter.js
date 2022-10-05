d96feaa8ed41948940df1e0c218647134f8f3275
ben <brunette.ben@gmail.com>
2022-10-05 18:04:19 +0200

    let's go

---
+++ audiosplitter.js
@@ -0,0 +1,7 @@
+// Open file
+// read file int buffer
+// get Extension tye
+// traverse buffer - get data from header if present
+// copy bytes from buffer to LEFT and RIGHT buffers depending on size of samples
+// write Left buffer to left file, and right to right
+// bonus - handle 8, 16, 32 bit?
